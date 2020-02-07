43d59814707b2f3db8a81b3adef93a53235bf8c6
Andrey Zhvakin <barmaglot@yandex-team.ru>
2020-02-07 23:23:48 +0300

    initial

---
+++ precache-manifest.100c6709d5123122aabf4503551b16b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016c680d82fe71c3b207e2fa7de72507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d636083aaac79e402ce4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21afb91e54d6022caf0b",
+    "url": "/static/js/2.259873b9.chunk.js"
+  },
+  {
+    "revision": "696e3b45bb13bbed1890acab7bc6b9aa",
+    "url": "/static/js/2.259873b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d636083aaac79e402ce4",
+    "url": "/static/js/main.d84da4f8.chunk.js"
+  },
+  {
+    "revision": "c0c4ae14e6229d26b828",
+    "url": "/static/js/runtime-main.8ebdd70e.js"
+  }
+]);
